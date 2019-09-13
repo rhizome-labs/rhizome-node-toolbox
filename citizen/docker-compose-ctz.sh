2bd97e773fcc3c3822f8cf2bc067d4d1111f788e
RHIZOME <53635700+rhizomeicx@users.noreply.github.com>
2019-09-13 14:56:45 +0900

    Create docker-compose-ctz.sh

---
+++ citizen/docker-compose-ctz.sh
@@ -0,0 +1,2 @@
+curl -o /home/rhizome/prep/docker-compose.yml https://raw.githubusercontent.com/rhizomeicx/rhizome-node-toolbox/master/prep/docker-compose.yml > /dev/null 2>&1
+cd /home/rhizome/prep && docker-compose up -d
